0c963e9c25dcae12f35e05a57c9deb35d3b4d5a6
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-10-06 13:43:45 +1100

    Add project files.

---
+++ BeanSceneMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BeanSceneMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
