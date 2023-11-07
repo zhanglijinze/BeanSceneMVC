29b3a4444572a5ec0249a6a092f0f564fbacd703
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-07 11:10:21 +1100

    added scaffolded controller and view for menu items

---
+++ BeanSceneMVC/Controllers/MenuItemsController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BeanSceneMVC.Data;
+using BeanSceneMVC.Models;
+
+namespace BeanSceneMVC.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuItemsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MenuItems
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.MenuItems.Include(m => m.MenuCategory);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: MenuItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.MenuItems == null)
+            {
+                return NotFound();
+            }
+
+            var menuItem = await _context.MenuItems
+                .Include(m => m.MenuCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Create
+        public IActionResult Create()
+        {
+            ViewData["MenuCategoryId"] = new SelectList(_context.MenuCategories, "Id", "Name");
+            return View();
+        }
+
+        // POST: MenuItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Price,Photo,IsGlutenFree,IsDiaryFree,IsVegetarian,IsVegan,IsAllergenFree,MenuCategoryId")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menuItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuCategoryId"] = new SelectList(_context.MenuCategories, "Id", "Name", menuItem.MenuCategoryId);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.MenuItems == null)
+            {
+                return NotFound();
+            }
+
+            var menuItem = await _context.MenuItems.FindAsync(id);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+            ViewData["MenuCategoryId"] = new SelectList(_context.MenuCategories, "Id", "Name", menuItem.MenuCategoryId);
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Photo,IsGlutenFree,IsDiaryFree,IsVegetarian,IsVegan,IsAllergenFree,MenuCategoryId")] MenuItem menuItem)
+        {
+            if (id != menuItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menuItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuItemExists(menuItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MenuCategoryId"] = new SelectList(_context.MenuCategories, "Id", "Name", menuItem.MenuCategoryId);
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.MenuItems == null)
+            {
+                return NotFound();
+            }
+
+            var menuItem = await _context.MenuItems
+                .Include(m => m.MenuCategory)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.MenuItems == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.MenuItems'  is null.");
+            }
+            var menuItem = await _context.MenuItems.FindAsync(id);
+            if (menuItem != null)
+            {
+                _context.MenuItems.Remove(menuItem);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuItemExists(int id)
+        {
+          return (_context.MenuItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
