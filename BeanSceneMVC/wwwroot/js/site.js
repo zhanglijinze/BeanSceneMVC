e215519d838b3764152881d358e30dc969551d26
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-08 21:07:03 +1100

    add dark mode feature. toggle within one page.

--- BeanSceneMVC/wwwroot/js/site.js
+++ BeanSceneMVC/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle('hero');
+    document.body.classList.toggle('dark-mode');
+}
