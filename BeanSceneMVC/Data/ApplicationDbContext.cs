0539a907c4427f7dbce32066e4a402575500c400
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-09 09:01:01 +1100

    add area model with EF migrations.

--- BeanSceneMVC/Data/ApplicationDbContext.cs
+++ BeanSceneMVC/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace BeanSceneMVC.Data
     {
         public DbSet<MenuCategory> MenuCategories{ get; set; }
         public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Area> Areas { get; set; }  
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
         { 
         
---
+++ BeanSceneMVC/Models/Area.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeanSceneMVC.Models
+{
+    [Index(nameof(Name),IsUnique =true)]
+    public class Area
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(5,MinimumLength =2)]
+        public string Name { get; set; }
+
+        public Area() { Name = ""; }
+
+        public Area(string name) 
+        {
+            Name = name;        
+        }
+
+    }
+}
