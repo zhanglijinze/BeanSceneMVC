c104a2e8f61ab81089035a4d2a95add82cd39eb1
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-10 14:46:47 +1100

    scaffold area and table for controller and view

---
+++ BeanSceneMVC/Controllers/AreasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BeanSceneMVC.Data;
+using BeanSceneMVC.Models;
+
+namespace BeanSceneMVC.Controllers
+{
+    public class AreasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AreasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Areas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Areas != null ? 
+                          View(await _context.Areas.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Areas'  is null.");
+        }
+
+        // GET: Areas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Areas == null)
+            {
+                return NotFound();
+            }
+
+            var area = await _context.Areas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (area == null)
+            {
+                return NotFound();
+            }
+
+            return View(area);
+        }
+
+        // GET: Areas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Areas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Area area)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(area);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(area);
+        }
+
+        // GET: Areas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Areas == null)
+            {
+                return NotFound();
+            }
+
+            var area = await _context.Areas.FindAsync(id);
+            if (area == null)
+            {
+                return NotFound();
+            }
+            return View(area);
+        }
+
+        // POST: Areas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Area area)
+        {
+            if (id != area.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(area);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AreaExists(area.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(area);
+        }
+
+        // GET: Areas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Areas == null)
+            {
+                return NotFound();
+            }
+
+            var area = await _context.Areas
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (area == null)
+            {
+                return NotFound();
+            }
+
+            return View(area);
+        }
+
+        // POST: Areas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Areas == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Areas'  is null.");
+            }
+            var area = await _context.Areas.FindAsync(id);
+            if (area != null)
+            {
+                _context.Areas.Remove(area);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AreaExists(int id)
+        {
+          return (_context.Areas?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ BeanSceneMVC/Controllers/TablesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BeanSceneMVC.Data;
+using BeanSceneMVC.Models;
+
+namespace BeanSceneMVC.Controllers
+{
+    public class TablesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TablesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tables
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Tables.Include(t => t.Area);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Tables/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Tables == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables
+                .Include(t => t.Area)
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return View(table);
+        }
+
+        // GET: Tables/Create
+        public IActionResult Create()
+        {
+            ViewData["AreaId"] = new SelectList(_context.Areas, "Id", "Name");
+            return View();
+        }
+
+        // POST: Tables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Code,AreaId")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(table);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AreaId"] = new SelectList(_context.Areas, "Id", "Name", table.AreaId);
+            return View(table);
+        }
+
+        // GET: Tables/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Tables == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables.FindAsync(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+            ViewData["AreaId"] = new SelectList(_context.Areas, "Id", "Name", table.AreaId);
+            return View(table);
+        }
+
+        // POST: Tables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Code,AreaId")] Table table)
+        {
+            if (id != table.Code)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(table);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TableExists(table.Code))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AreaId"] = new SelectList(_context.Areas, "Id", "Name", table.AreaId);
+            return View(table);
+        }
+
+        // GET: Tables/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Tables == null)
+            {
+                return NotFound();
+            }
+
+            var table = await _context.Tables
+                .Include(t => t.Area)
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return View(table);
+        }
+
+        // POST: Tables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Tables == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Tables'  is null.");
+            }
+            var table = await _context.Tables.FindAsync(id);
+            if (table != null)
+            {
+                _context.Tables.Remove(table);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TableExists(string id)
+        {
+          return (_context.Tables?.Any(e => e.Code == id)).GetValueOrDefault();
+        }
+    }
+}
