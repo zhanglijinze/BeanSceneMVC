553fcaca8b55a8c1970b4b104f83e017088a4552
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-10 22:05:15 +1100

    Add sittingtype and timeslot models

---
+++ BeanSceneMVC/Models/SittingType.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeanSceneMVC.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class SittingType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Must be 2-20 characters")]
+
+        public string Name { get; set; } = null!;
+
+    }
+}
---
+++ BeanSceneMVC/Models/Timeslot.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeanSceneMVC.Models
+{
+    public class Timeslot
+    {
+        
+        [Key]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm tt}")]
+        public DateTime Time { get; set; }
+
+        [NotMapped]
+        public string TimeFormatted { get => Time.ToString("hh:mm tt"); }
+    }
+
+}
