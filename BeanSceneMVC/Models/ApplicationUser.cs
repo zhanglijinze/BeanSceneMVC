e71d0bc65211a314a90ad2122537a632e4759446
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-12 14:37:08 +1100

    add custom identityuser model - applicationUser

---
+++ BeanSceneMVC/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BeanSceneMVC.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        [NotMapped]
+        public string FullName { get => FirstName + " " + LastName; }
+
+    }
+}
