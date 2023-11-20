bd93555389c295976ffd5141f9a0876ed811ca71
Jinze Zhang <jinze.zhang@studytafensw.edu.au>
2023-11-20 20:08:11 +1100

    maintain dark mode or light mode across all pages

--- BeanSceneMVC/wwwroot/js/site.js
+++ BeanSceneMVC/wwwroot/js/site.js
@@ -2,8 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function toggleDarkMode() {
+/*function toggleLightMode() {
     const body = document.body;
     body.classList.toggle('hero');
-    document.body.classList.toggle('dark-mode');
+    document.body.classList.toggle('light-mode');
+}*/
+
+// Function to apply the mode
+function applyMode(isLightMode) {
+    const body = document.body;
+    if (isLightMode) {
+        body.classList.remove('hero'); // Apply light mode (black text)
+        body.classList.add('light-mode');
+        
+    } else {
+        body.classList.remove('light-mode');
+        body.classList.add('hero'); // Apply dark mode (white text)
+    }
 }
+
+// Function to toggle light mode and save the state
+function toggleLightMode() {
+    const isLightMode = document.body.classList.contains('light-mode');
+    localStorage.setItem('lightMode', JSON.stringify(!isLightMode));
+    applyMode(!isLightMode);
+}
+
+// Apply mode as soon as the page is loaded
+(function () {
+
+    const savedMode = JSON.parse(localStorage.getItem('lightMode'));
+    // Default to dark mode (white text) if no mode is set
+    const isLightMode = savedMode !== null ? savedMode : false;
+    applyMode(isLightMode);
+})();
